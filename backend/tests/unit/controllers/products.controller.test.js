27c9650d2300f9fb4b4c8f44725d8950a00f6c46
Julio Matias <juliocesark2@hotmail.com>
2024-02-23 15:45:36 -0300

    test: implementando estrutura inicial test/controller/products

---
+++ backend/tests/unit/controllers/products.controller.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+// const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+// const { expect } = chai;
+chai.use(sinonChai);
+
+describe('Testando o controller de products', function () {
+  it('Testa se na rota GET /products todos os produtos são retornados', async function () {
+    // triploA = 'Arrange, Act, Assert';
+    // Arrange
+
+    // Act
+
+    // Assert
+  });
+});
