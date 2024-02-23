8ffbc058eaf2cf287a1fb5442e6b678f66c793a6
Julio Matias <juliocesark2@hotmail.com>
2024-02-23 17:43:09 -0300

    data: instalando comelize

---
+++ backend/tests/unit/models/products.model.test.js
@@ -0,0 +1,4 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const connection = require('../../../src/models/connection');
+const { productsModel } = require('../../../src/models');
