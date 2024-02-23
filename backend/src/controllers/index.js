338d8c75cf31f9b20a00481f0c3acd2c8e248aee
Julio Matias <juliocesark2@hotmail.com>
2024-02-23 14:56:40 -0300

    feat: implementado controller, products

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,5 +1,7 @@
 const salesController = require('./sales.controller');
+const productsController = require('./products.controller');
 
 module.exports = {
   salesController,
+  productsController,
 };
