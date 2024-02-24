fdfec110e0175724c48abe57a10abbd975edad71
Julio Matias <juliocesark2@hotmail.com>
2024-02-24 14:28:37 -0300

    refactor: modificando posição da rota no app para ver se passa no teste avaliador da trybe

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,12 @@ const { productsRoutes } = require('./routes');
 
 const app = express();
 
+app.use(express.json());
+app.use('/products', productsRoutes);
+
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.json({ status: 'Store Manager UP!' });
 });
-
-app.use(express.json());
-app.use('/products', productsRoutes);
 
 module.exports = app;
