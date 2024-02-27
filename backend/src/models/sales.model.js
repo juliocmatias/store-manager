90236c3ef2c952d94e311ff89ac4f01be45d20fd
Julio Matias <juliocesark2@hotmail.com>
2024-02-27 09:41:50 -0300

    feat: implementando cadastro de venda com a insert da salesModel

--- backend/src/models/sales.model.js
+++ backend/src/models/sales.model.js
@@ -1,5 +1,7 @@
 const camelize = require('camelize');
 const connection = require('./connection');
+const { getFormattedColumnNames, 
+  getFormattedPlaceholders } = require('../utils/generateFormattedQuery');
 
 const getAllFromDB = async () => {
   const [sales] = await connection.execute(
@@ -29,20 +31,60 @@ const findById = async (id) => {
   );
   return camelize(sale);
 };
+
+// insert é uma função que recebe um array de objetos, onde cada objeto é um produto
+// vendido, contendo o id do produto e a quantidade vendida
+// Exemplo:
+// [
+//   {
+//     "productId": 1,
+//     "quantity": 1
+//   },
+//   {
+//     "productId": 2,
+//     "quantity": 5
+//   }
+// ]
+// A função deve inserir a venda no banco de dados e retornar um objeto com o id da
+// venda e os produtos vendidos.
+// o retorno do insert é um objeto, onde deve conte o id da venda
+// e itemsSold, que é um array de objetos, onde cada objeto é um produto
+// vendido, contendo o id do produto e a quantidade vendida
+// Exemplo:
+// {
+//   "id": 3,
+//   "itemsSold": [
+//     {
+//       "productId": 1,
+//       "quantity": 1
+//     },
+//     {
+//       "productId": 2,
+//       "quantity": 5
+//     }
+//   ]
+// }
+
 const insert = async (sales) => {
-  // insertSaleId pega o id da venda que foi inserida 
-  const insertSaleId = [];
-  sales.forEach(async (sale) => {
-    const currentDate = new Date();
-    const [result] = await connection.execute(
-      'INSERT INTO sales (date) VALUES (?);',
-      [currentDate],
+  const [{ saleId }] = await connection.execute(
+    'INSERT INTO sales (date) VALUES (?)',
+    [new Date()],
+  );
+  sales.forEach(async ({ productID, quantity }) => {
+    const columns = getFormattedColumnNames({ saleId, productID, quantity });
+    const placeholders = getFormattedPlaceholders({ saleId, productID, quantity });
+    const query = `INSERT INTO sales_products (${columns}) VALUES (${placeholders})`;
+    await connection.execute(
+      query,
+      [saleId, productID, quantity],
     );
-    insertSaleId.push(result.insertId);
   });
+
+  return { id: saleId, itemsSold: sales };
 };
 
 module.exports = {
   getAllFromDB,
   findById,
+  insert,
 };
